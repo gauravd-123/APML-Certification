418c3bfa6f9f20efee0336cff4427421b29b5768
Gaurav Dabholkar <reckon654@gmail.com>
2022-01-29 14:06:39 +0530

    Updates

---
+++ precache-manifest.6d1e9a79c9f3a4cf8ecc36b82b4ccbca.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ca61a74aaf959703c88",
+    "url": "/APML-Certification/static/css/main.1cab5cbc.chunk.css"
+  },
+  {
+    "revision": "6ca61a74aaf959703c88",
+    "url": "/APML-Certification/static/js/main.57ed0b0c.chunk.js"
+  },
+  {
+    "revision": "d733040286ba41413e5f",
+    "url": "/APML-Certification/static/js/runtime~main.565c1a78.js"
+  },
+  {
+    "revision": "7aba343a4b3a1799212a",
+    "url": "/APML-Certification/static/css/2.fadb9d42.chunk.css"
+  },
+  {
+    "revision": "7aba343a4b3a1799212a",
+    "url": "/APML-Certification/static/js/2.e6b21d5b.chunk.js"
+  },
+  {
+    "revision": "e976df5a19bda2e73b24f1dcdff79f1c",
+    "url": "/APML-Certification/static/media/b1.e976df5a.jpg"
+  },
+  {
+    "revision": "8f5bf972709782bb687f4785b5589020",
+    "url": "/APML-Certification/static/media/hiw.8f5bf972.jpg"
+  },
+  {
+    "revision": "5cc5e014eff162b3be400879f9d13684",
+    "url": "/APML-Certification/static/media/b3.5cc5e014.jpg"
+  },
+  {
+    "revision": "df82856f76ae02bc7da9d97b5561a51f",
+    "url": "/APML-Certification/static/media/cert.df82856f.png"
+  },
+  {
+    "revision": "27b83fdfd380a16fcf4aa5097ce24067",
+    "url": "/APML-Certification/index.html"
+  }
+];
